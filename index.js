550d0e732b67a28ad9924d6395821987c05dcd8d
wns312 <wns312@gmail.com>
2020-07-01 00:04:15 +0900

    jwt로그인까지완료

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const cookieParser= require('cookie-parser');
 const app = express();
 const port = 3000;
 //{User}로 가져오는 이유는 export를 {User}로 해주었기 때문이다.
@@ -11,50 +12,59 @@ const {mongoURI} = require('./config/key')
 app.use(bodyParser.urlencoded({extended : true}));
 //application/json 타입 데이터를 분석해 가져올 수 있게 해준다.
 app.use(bodyParser.json());
+//쿠키파서 사용설정
+app.use(cookieParser());
+
 
 mongoose.connect(mongoURI, {
-    useNewUrlParser : true, useUnifiedTopology : true, useCreateIndex : true, useFindAndModify : false
+  useNewUrlParser : true, useUnifiedTopology : true, useCreateIndex : true, useFindAndModify : false
 })
 .then(()=>{ console.log("connected")}) 
 .catch((err)=>{console.log(err)}) 
 
 app.get('/', (req,res)=>{
-    res.send('Hello world')
+  res.send('Hello world')
 })
 
-app.post('/register', (req, res)=>{
+app.post('api/users/register', (req, res)=>{ 
+  const user = new User(req.body); 
+  user.save((err, doc)=>{ 
+    if(err) {return res.json({ success : false, err})} 
     
-    const user = new User(req.body); 
-    user.save((err, doc)=>{ 
-        if(err) {return res.json({ success : false, err})} 
-        
-        return res.status(200).json({
-            success : true
-        })
+    return res.status(200).json({
+      success : true
+    })
+  });
+})
+
+app.post("api/users/login", (req, res) => {
+  User.findOne({ email: req.body.email }, function (err, user) {
+    if (!user)
+      return res.json({
+        loginSuccess: false,
+        message: "이메일에 해당하는 유저가 없습니다",
+      });
+
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 일치하지 않습니다",
+        });
+      }
+      //3. 일치시 유저를 위한 토큰을 생성한다
+      // 다시 스키마에 generateToken함수를 만들어준다.
+      user.generateToken((err, user) => {
+        //이제 여기에는 유저 정보가 담겨있다
+        if (err) return res.status(400).send(err); //400은 에러가 있다는 뜻
+        //토큰을 저장한다 어디에?? 1) 쿠키에 2) 로컬스토리지.. 우리는 쿠키에 저장할 것
+        res.cookie("x_auth", user.token) // x_auth라는 이름으로 쿠키가 들어감
+        .status(200)  //성공 신호보내주고
+        .json({ loginSuccess : true, userId : user._id }) // JSON으로 로그인 성공 변수와 ObjectID를 userId라는 이름으로 보내줌
+      });
     });
-})
-
-app.post('./login', (req, res)=>{
-    User.findOne({email : req.body.email}, function (err, user) {
-        if(!err) return res.json({ loginSuccess : false, err })
-        if(!user) return res.json({ loginSuccess : false, message : "이메일에 해당하는 유저가 없습니다"})
-
-        user.comparePassword(req.body.comparePassword, (err, isMatch)=>{
-            if(!err) return res.json({loginSuccess : false, err});
-            if(!isMatch) {
-                return res.json({
-                    loginSuccess : false,
-                    message : "비밀번호가 일치하지 않습니다"
-                })
-            }
-    //3. 일치시 유저를 위한 토큰을 생성한다
-    // 다시 스키마에 generateToken함수를 만들어준다.
-            user.generateToken((err, user)=>{
-
-            });
-        })
-    }) 
-})
+  });
+});
 
 app.listen(port, ()=>{
     console.log(`http://localhost:${port}`);
--- models/User.js
+++ models/User.js
@@ -4,68 +4,72 @@ const jwt = require('jsonwebtoken');
 const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
-    name : { // 이름
-        type : String,
-        maxlength : 50
-    },
-    email : { // 유일값으로 설정
-        type : String,
-        trim : true, // 앞뒤공백제거
-        unique : 1 // 유일값
-    },
-    password : {
-        type : String,
-        minLength : 5
-    },
-    role : { // 관리자와 일반사용자 구분을 위해
-        type : Number, //0이면 일반, 관리자면 1 처럼 구분
-        default : 0 // 따로 정하지 않으면 role을 0을 준다
-    },
-    image : String, // 프로필이미지
-    token : { // 토큰
-        type : String,
-    },
-    tokenExp : { // 토큰 유효시간
-        type : Number
-    }
-})
+  name: {
+    // 이름
+    type: String,
+    maxlength: 50,
+  },
+  email: {
+    // 유일값으로 설정
+    type: String,
+    trim: true, // 앞뒤공백제거
+    unique: 1, // 유일값
+  },
+  password: {
+    type: String,
+    minLength: 5,
+  },
+  role: {
+    // 관리자와 일반사용자 구분을 위해
+    type: Number, //0이면 일반, 관리자면 1 처럼 구분
+    default: 0, // 따로 정하지 않으면 role을 0을 준다
+  },
+  image: String, // 프로필이미지
+  token: {
+    // 토큰
+    type: String,
+  },
+  tokenExp: {
+    // 토큰 유효시간
+    type: Number,
+  },
+});
 userSchema.pre('save', function(next){
-    let user = this; 
+  let user = this;
 
-    if(user.isModified('password')) {   
-        bcrypt.genSalt(saltRounds, function(err, salt) {
-            if (err) return next(err); 
-            bcrypt.hash(user.password, salt, function(err, hash) {
-                if(err) return next(err);
-                user.password = hash
-                next();
-            });
-        });
-    }else{
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
         next();
-    }
+      });
+    });
+  } else {
+    next();
+  }
 })
 
 
-userSchema.methods.comparePassword = function(plainPassword, callback){
-    bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
-        if(err) return callback(err); 
-        callback(null, isMatch);
-    })
-}
+userSchema.methods.comparePassword = function (plainPassword, callback) {
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return callback(err) 
+    callback(null, isMatch);
+  });
+};
 
 userSchema.methods.generateToken = function (callback) {
-    //jwt을 이용해서 토큰 생성하기
-    let user = this; //마찬가지로 입력 정보를 this로 가져온다 
-    let token = jwt.sign(user._id, "secrettoken") // 여기서의 id는 고유값인 _id : ObjectId("...") 를 말한다
-    //이렇게 sign에 고유값_id와 문장을 넣어주면 이 둘을 더해서 토큰을 만들어준다
-    //추후에 jwt 조회를 할 시 ObjectId를 받아와서 문장을 더해 토큰화 해서 둘을 비교하는 방식으로 인증한다
-
-    user.token = token; // 로그인유저의 token정보에 암호화된 token을 넣어준다
-}
-
-
-
+  
+  let user = this; JSON.stringify()
+  let token = jwt.sign(user._id.toHexString(), "secretToken"); 
+  user.token = token; 
+  user.save(function (err, user) {
+    
+    if (err) return callback(err);
+    callback(null, user); 
+  });
+};
 
 const User = mongoose.model('User',userSchema ) // (모델의 이름, 스키마)
 module.exports = {User}
