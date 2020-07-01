3e358bf09a8883b2dd9a480e1631e9c390a715c0
wns312 <wns312@gmail.com>
2020-07-01 13:43:44 +0900

    프록시 수정 및 antd 설치

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,10 +1,10 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(
     '/api',
-    createProxyMiddleware({
+    proxy({
       target: 'http://localhost:5000',
-      changeOrigin: true,
+      changeOrigin: true
     })
   );
 };
